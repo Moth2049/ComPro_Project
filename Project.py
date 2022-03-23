1fa7587cad411c1b8b1fe8296e8439d63e49e6d5
Moth2049 <43887405+Moth2049@users.noreply.github.com>
2022-03-23 19:00:05 +0700

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,16 @@
+#Menu Function
+while True :
+    item = int(input("Please select the menu : "))
+    if item == 1 :
+        Enter_item()
+    if item == 2 :
+        Display()
+    if item == 3 :
+        Save() 
+    if item == 4 :
+        Load()
+    if item == 5 :
+        Delete()
+    if item == 6 :
+        
+ 
