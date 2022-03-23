f55abfba2a115e4fd528a0f9ec146884a9443ab4
Moth2049 <43887405+Moth2049@users.noreply.github.com>
2022-03-23 21:24:33 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,16 +1,15 @@
 #Menu Function
+from Enter_item import EI #Function1
+
 while True :
+    print("1. Enter item")
+    print("2. Display")
+    print("3. Save")
+    print("4. Load")
+    print("5. Delete")
     item = int(input("Please select the menu : "))
+    
     if item == 1 :
-        Enter_item()
-    if item == 2 :
-        Display()
-    if item == 3 :
-        Save() 
-    if item == 4 :
-        Load()
-    if item == 5 :
-        Delete()
-    if item == 6 :
-        
+        EI()
+
  
