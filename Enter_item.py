0ef6f8934b926297dbf81b46aed618ca639bb9d2
Moth2049 <43887405+Moth2049@users.noreply.github.com>
2022-03-27 14:20:18 +0700

    Update Enter_item.py

--- Enter_item.py
+++ Enter_item.py
@@ -1,8 +1,15 @@
 def EI():
     A = str(input("what do u need : "))
     Date = A.partition(" ")[0]
-    Money = A.partition(" ")[1]
-    DT = A.partition(" ")[2] #Description
+    Money = A.partition(" ")[2]
+    Cash = Money.partition(" ")[0] 
+    DT = Money.partition(" ")[2] #Description
+    
+    Day = Date.partition("/")[0]
+    Month = Date.partition("/")[1]
+    Year = Date.partition("/")[2]
     print(Date)
-    print(Money)
+    print(Cash)
     print(DT)
+    
+
