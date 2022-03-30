568207266791ba81fd6652b59c32ce6c022c9c92
Moth2049 <43887405+Moth2049@users.noreply.github.com>
2022-03-30 09:22:17 +0700

    Update Enter_item.py

--- Enter_item.py
+++ Enter_item.py
@@ -1,10 +1,21 @@
 def EI():
     #Extract word from sentense Complete!
-    A = str(input("what do u need : "))
-    Date = A.partition(" ")[0]
-    Money = A.partition(" ")[2]
-    Cash = Money.partition(" ")[0] 
-    DT = Money.partition(" ")[2] #Description
+    Word = str(input("what do u need : "))
+    Date = Word.partition(" ")[0]
+    Money_DT = Word.partition(" ")[2]
+    Money = Money_DT.partition(" ")[0] 
+    DT = Money_DT.partition(" ")[2] #Description
+    if "+" in Money :
+        CashP = Money.partition("+")[2]
+        FileCP = open("CashP.txt","a+")
+        FileCP.write(CashP+"\n")
+        FileCP.close
+    elif "-" in Money :
+        CashM = Money.partition("-")[2]
+        FileCM = open("CashM.txt","a+")
+        FileCM.write(CashM+"\n")
+        FileCM.close
+
 
     #Extract Date Complete!
     Day = Date.partition("/")[0]
@@ -12,7 +23,13 @@ def EI():
     Month = MY.partition("/")[0]
     Year = MY.partition("/")[2]
     
-    #Write flie Complete!
-    file = open("Data.txt","r+")    #Read and write at the same time
-    file.write(A + "\n")
+    FileD = open("Date.txt","a+")
+    FileD.write(Date+"\n")
+    FileD.close
     
+    #Write flie Complete!
+    file = open("Data.txt","a+")    #Read and write at the same time
+    file.write(Word+"\n")
+    file.close
+    
+   
