8f3f96abb2a336cd55409da0ce5ab0f0efa4732e
Moth2049 <43887405+Moth2049@users.noreply.github.com>
2022-04-05 22:05:31 +0700

    Update Display.py

--- Display.py
+++ Display.py
@@ -1,23 +1,58 @@
 import pandas as pd
+import os
+import re
 
 def Display():
-    
-    date=input('วันที่ต้องการค้นหา:')
-    file = ("Data.txt","a+")
-    with open(file) as f :
-        line = f.readline()
-        cnt = 1
-        while line:
-
-        a = 200
-        b = 100
-        describe = "k"
+    date=input('วันที่ที่ต้องการค้นหา:')
+    file = open("Data.txt","r")
+    count = 0
+    line = 0
+    x = file.readlines()
+    b = len(x)
+    CashM = []
+    CashP = []
+    Datelist = []
+    DTlist = []
+    Total =[]
+    #print(x[count])
+    #print(x[b-1])
+    for i in range(1,b+1):
+        file = open("Data.txt","r")
+        Sentense = file.read().splitlines()
+        SentenseCount = Sentense[count]
+        StrSentense = str(SentenseCount)
+        Date = StrSentense.split(" ")[0]
+        if date == Date :
+            Datelist.append(Date)
+            Money_DT = StrSentense.partition(" ")[2]
+            Money = Money_DT.partition(" ")[0] 
+            DT = Money_DT.partition(" ")[2] #Description
+            DTlist.append(DT)
+            print(Money_DT)
+            print(DT)
+            if "+" in Money:
+                CashPl = Money.partition("+")[2]
+                CashP.append(CashPl)
+                CashMi = 0
+                CashM.append(CashMi)
+                
+            elif "-" in Money:
+                CashMi = Money.partition("-")[2]
+                CashM.append(CashMi)
+                CashPl = 0
+                CashP.append(CashPl)
+            total = "-"
+            Total.append(total)    
+        count += 1
     colsname = {
-        'Date' : [date],
-        'Description' : [describe],
-        'Income' : [a],
-        'Expense' : [b],
-        'total' : [a-b]
-
-    }
+        'Date' : Datelist,
+        'Description' : DTlist,
+        'Income' : CashP,
+        'Expense' : CashM,
+        'total' : Total
+        }
     print(pd.DataFrame(colsname))
+
+
+while True :
+    Display()
