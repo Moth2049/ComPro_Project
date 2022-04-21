501659e41749b4418089d147a52ab23109960409
Moth2049 <43887405+Moth2049@users.noreply.github.com>
2022-04-21 16:49:16 +0700

    Update Enter_item.py

--- Enter_item.py
+++ Enter_item.py
@@ -1,29 +1,6 @@
-import os
 def EI():
 
-    #Extract word from sentense Complete!
     Word = str(input("what do u need : "))
-    Date = Word.partition(" ")[0]
-    Money_DT = Word.partition(" ")[2]
-    Money = Money_DT.partition(" ")[0] 
-    DT = Money_DT.partition(" ")[2] #Description
-    if "+" in Money:
-        Cash = Money.partition("+")[2]
-    if "-" in Money:
-        Cash = Money.partition("-")[2]
-    #Extract Date Complete!
-    Day = Date.partition("/")[0]
-    MY = Date.partition("/")[2] #Extract Month and Year
-    Month = MY.partition("/")[0]
-    Year = MY.partition("/")[2]
-    
-    #Write flie Complete!
-    file = open("Data.txt","a+")    #Read and write at the same time
+    file = open("Data.txt","a+")    #Write at new line
     file.write(Word+"\n")
     file.close
-
-while True :
-    EI()
-    
-    
-   
